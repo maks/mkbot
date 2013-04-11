f35e162ae7b81dc5c1d966399e85dd4ee3e79f6b
Maksim Lin <maks@manichord.com>
2013-04-11 22:05:01 +1000

    add httpd to serve log files
    also write log to seperate files, 1 per day. Basic doc in readme.
    git to ignore the log dir

---
+++ README.md
@@ -0,0 +1,10 @@
+# MKBot - A very basic irc bot
+
+## Usage
+
+Everything hardcoded in the js so just:
+
+```node mkbot.js```
+
+For now only does logging. Log available at http://servername:port/logs
+
--- mkbot.js
+++ mkbot.js
@@ -1,10 +1,69 @@
-var irc = require('irc');
-var client = new irc.Client('irc.freenode.net', 'jsgitbot', {
-    channels: ['#js-git'],
-});
+/*jshint node:true */
+"use strict";
+
+var fs = require('fs'),
+    http = require('http'),
+    ecstatic = require('ecstatic'),
+    irc = require('irc'),
+    strftime = require('strftime'),
+    channel = '#js-git',
+    botname = 'mkbot',
+    PORT = 8001,
+    HOST = 'manichord.com',
+    httpdOpts = {
+             root       : __dirname + '/public', 
+             baseDir    : '/',
+             cache      : 1,
+             showDir    : true,
+             autoIndex  : true,
+             defaultExt : 'html', 
+             gzip       : false
+           },     
+    client = new irc.Client('irc.freenode.net', botname, {
+       channels: [channel]
+    });
+
+http.createServer(
+  ecstatic(httpdOpts)
+).listen(PORT);
+
 
 client.addListener('message', function (from, to, message) {
-    var dts = new Date().toISOString();
-    console.log('['+dts+'] '+from + ' => ' + to + ': ' + message);
+    toLog(from + ' => ' + to + ': ' + message);
 });
+
+client.addListener('pm', function (from, message) {
+    console.log(from + ' => ME: ' + message);
+});
+
+client.addListener('error', function(message) {
+    console.error('error: ', message);
+});
+
+client.addListener('registered', function(message) {
+   console.log('registered ok');
+   announceMyself();
+});
+
+client.addListener('join', function(channel, nick, message) {
+   toLog(nick + ' has joined '+channel);
+   announceMyself();
+});
+
+function toLog(str) {
+    var now = new Date(),
+    	dts = strftime('%F %T %z', new Date()),
+    	filename = __dirname+'/public/logs/'+strftime('%F')+".txt";
+
+    fs.appendFile(filename, '['+dts+'] '+str+"\n", function (err) {
+       if (err) {
+           console.error('error appending to logfile '+filename+' '+err);
+       }
+    });
+}
+
+function announceMyself() {
+    client.say(channel, "Hi I'm "+botname+" and I'm logging this channel. Log archive at: http://"+HOST+':'+PORT+'/logs/');
+}
+
 
